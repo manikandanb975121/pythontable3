eee66ae3ad8e70e5181b16e06ff621d68bfc1c0b
manikandanb975121 <36506866+manikandanb975121@users.noreply.github.com>
2020-06-04 10:38:45 +0530

    Add files via upload

---
+++ myapp/config.py
@@ -0,0 +1,5 @@
+graph_auth = {
+    'tenant': '72fd5508-82d2-481c-bc6a-36aca7da0d20',
+    'client_id': '90375edd-934d-4472-a171-7ac9104939c5',
+    'client_secret': 'yUXhHGmW1d_XiuC2W2l3P_5toGH~dB39~3',
+}
